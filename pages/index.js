c12016eda20cddaa571737b1094b3f31b7d0b0a9
GGolfz <wisarutgolf11@gmail.com>
2021-02-22 23:37:23 +0700

    add: mockup index page (for answer) :sparkles:

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,49 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+import React, { Fragment, useEffect, useState } from "react";
+import io from "socket.io-client";
+let socket;
+const Home = () => {
+  const [question, setQuestion] = useState(null);
+  const [answer, setAnswer] = useState("");
+  const [isAns, setIsAns] = useState(false);
+  useEffect(() => {
+    socket = io();
+    socket.emit("fetch-data");
+    socket.on("get-data", (data) => {
+      setQuestion(data);
+      console.log(data);
+    });
+    socket.on("clear", () => {
+      setQuestion(null);
+    });
+  }, []);
+  const handleAnswer = () => {
+    if (question) {
+      socket.emit("answer", {
+        questionId: question.questionId,
+        answer: answer.toLowerCase().trim(),
+      });
+      setIsAns(true)
+    }
+  };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <Fragment>
+      {question && !isAns ? (
+        <div>
+          <div>{question.questionName}</div>
+          <div>
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+            />
+            <button onClick={handleAnswer}>submit</button>
+          </div>
         </div>
-      </main>
+      ) : (
+        <div>No data now :(</div>
+      )}
+    </Fragment>
+  );
+};
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default Home;
