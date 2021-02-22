5075513b2cb125db4a15543ba4ff38636ccccd44
GGolfz <wisarutgolf11@gmail.com>
2021-02-23 00:11:32 +0700

    fix: port :hammer:

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 var cloudData = [];
 var currentId = null;
 const adminKey = '123456'
