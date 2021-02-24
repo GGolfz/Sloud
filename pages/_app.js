10fe7fac5223e4d19245dcd3e8e014c24039f1e3
GGolfz <wisarutgolf11@gmail.com>
2021-02-24 23:21:49 +0700

    install: nunito fonts :rocket:

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import Head from "next/head";
+import {Fragment} from 'react';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Sloud App</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
